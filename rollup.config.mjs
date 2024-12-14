66c8edfff9ff79008484e8075a803bb0e41d8a53
ParkerTomatoes <parker.tomatoes@gmail.com>
2024-12-14 13:40:48 -0500

    Bundle typescript definitions and fix sourcemaps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,13 @@
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import copy from 'rollup-plugin-copy';
+import dts from 'rollup-plugin-dts';
+import del from 'rollup-plugin-delete';
+
+const rebaseDist = relPath => relPath.startsWith('../')
+    ? relPath.substring(1)
+    : relPath;
+
 export default [{
     /* Library */
     input: './src/ds4qb-web.ts',
@@ -8,12 +15,14 @@ export default [{
         file: './dist/ds4qb-web.min.js',
         format: 'umd',
         name: 'ds4qb-web',
-        sourcemap: true
+        sourcemap: true,
+        sourcemapPathTransform: rebaseDist
     }, {
         file: './dist/ds4qb-web.min.mjs',
         format: 'es',
         name: 'ds4qb-web',
-        sourcemap: true
+        sourcemap: true,
+        sourcemapPathTransform: rebaseDist
     }],
     external: ['node:path', 'node:url', 'node:fs/promises'],
     plugins: [
@@ -21,17 +30,23 @@ export default [{
             rootDir: './src'
         }),
         terser({
-            sourceMap: {
-                includeSources: true, 
-                url: 'inline'
-            }
+            sourceMap: true
         }),
         copy({
             targets: [
                 { src: './README.md', dest: 'dist' },
-                { src: './package.dist.json', dest: 'dist', rename: 'package.json' }
+                { src: './package.dist.json', dest: 'dist', rename: 'package.json' },
+                { src: './src', dest: 'dist' }
             ]
         })
 
     ]
-}]
+}, {
+    input: './dist/ds4qb-web.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts(), del({
+        targets: ['./dist/*.d.ts', './dist/*.d.ts.map'],
+        hook: 'buildEnd'
+    })],
+},
+]
